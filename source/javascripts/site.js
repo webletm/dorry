d596d4c56b6b409ac14e9dccdd692ef858192774
Miroslav Naskovic <mirko@Miroslavs-MacBook-Pro.local>
2017-08-16 10:20:01 +1000

    first commit

---
+++ source/javascripts/site.js
@@ -0,0 +1,27 @@
+// This is where it all goes :)
+
+(function( root, $, undefined ) {
+	"use strict";
+	$(function () {
+		
+		$('#review-scroll').slick({
+			adaptiveHeight: true,
+			arrows: true
+		});
+
+		$('#nav a').on('click', function(a){
+			a.preventDefault();
+			var $el = $(this).attr('href');
+			$('html, body').animate({
+		        scrollTop: $($el).offset().top
+		    }, 1500);
+		});
+
+		$('#menu-btn').on('click', function(a){
+			a.preventDefault();
+			$('#nav').slideToggle(750 ,'easeOutBounce');
+		})
+
+	});
+
+} ( this, jQuery ));
