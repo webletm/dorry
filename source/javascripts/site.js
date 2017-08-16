fefb09fd8ff5ab62c447fecb85926f7e79e1d060
Miroslav Naskovic <miroslav@weavingweblets.com.au>
2017-08-16 12:37:39 +1000

    final changes

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -9,18 +9,35 @@
 			arrows: true
 		});
 
+		$('#gallery-scroller').slick({
+			adaptiveHeight: true,
+			arrows: true,
+			slidesToShow: 3,
+			centerPadding: '0px',
+			responsive: [
+			    {
+			      breakpoint: 480,
+			      settings: {
+			        centerMode: true,
+			        centerPadding: '0px',
+			        slidesToShow: 1
+			      }
+			    }
+			]
+		});
+
 		$('#nav a, #scrollerbtn').on('click', function(a){
 			a.preventDefault();
 			var $el = $(this).attr('href');
 			$('html, body').animate({
 		        scrollTop: $($el).offset().top
-		    }, 1500);
+		    }, 1500, 'easeOutBounce');
 		});
 
 		$('#menu-btn').on('click', function(a){
 			a.preventDefault();
 			$('#nav').slideToggle(750 ,'easeOutBounce');
-		})
+		});
 
 	});
 
